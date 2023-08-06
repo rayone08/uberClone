56ecd7b1f7cd38bc39d6a9a88fc7c8d5a5d33c78
Harish <harisiva.fb@gmail.com>
2023-08-06 20:06:39 +0530

    Live map at Home screen

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+import HomeScreen from './src/screens/HomeScreen'
+
+
+
+const App = () => {
+  return (
+    <View style = {styles.container}>
+      <HomeScreen/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+})
